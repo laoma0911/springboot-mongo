756a86532d1dc1b2d3f5287c8bf54e67ace5de34
mazhenbang <mazhenbang@gomeplus.com>
2017-04-07 10:04:43 +0800

    fix

--- src/main/java/com/laoma/mongodao/UserDao.java
+++ src/main/java/com/laoma/mongodao/UserDao.java
@@ -3,13 +3,14 @@ package com.laoma.mongodao;
 import com.laoma.base.BaseMongoDao;
 import com.laoma.base.impl.BaseMongoDAOImpl;
 import com.laoma.domain.User;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
  * @Auther: laoma
  * @create: 2017-04-06 下午12:01
  */
-@Service
+@Component
 public class UserDao extends BaseMongoDAOImpl<User> {
 
 }
